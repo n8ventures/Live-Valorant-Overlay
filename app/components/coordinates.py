93468bbea5428ac76b28ad0077290c0d6cce8411
John Nathaniel Calvara <n8ventures@gmail.com>
2022-07-13 16:30:41 +0800

    Fixed coordinates tool.

--- app/components/coordinates.py
+++ app/components/coordinates.py
@@ -42,7 +42,7 @@ def click_event(event, x, y, flags, params):
 if __name__=="__main__":
  
     # reading the image
-    img = cv2.imread('2.png')
+    img = cv2.imread('2.png', 1)
  
     # displaying the image
     cv2.imshow('image', img)
